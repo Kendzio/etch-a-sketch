d5be4cde76b261cca8b960e7406413af915ffec7
Konrad Zagozda <konradz1369@gmail.com>
2018-07-09 21:43:16 +0200

    grid elements changes color to black when hovered by mouse

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 my_grid = document.getElementById('grid');
 for (let i=0; i<256; i++){
 	let div = document.createElement('div');
+	div.classList.add("grid_elements")
 	my_grid.appendChild(div);
 }
+
+elements = document.querySelectorAll('div');
+
+elements.forEach(function(elem) {
+	elem.addEventListener("mouseenter", function() {
+		elem.classList.add("black");
+	})
+})
